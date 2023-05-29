07af1edd37ec6a4ad51367f96bcb8b9bfed27829
praveenkumatsekar <praveen0077kumar@gmail.com>
2023-05-30 03:53:09 +0530

    categories button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { CategoryComponent } from './category/category.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TodoComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
