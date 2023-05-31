e28e07ba2e4882fcbd56246119c2a198646a1e6a
praveenkumatsekar <praveen0077kumar@gmail.com>
2023-06-01 04:42:29 +0530

    category service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment';
+import { initializeApp } from 'firebase/app';
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { TodoComponent } from './todo/todo.component';
 import { CategoryComponent } from './category/category.component';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+/*import { AngularFireModule } from '@angular/fire';
+ */
+/* import { AngularFirestoreModule } from '@angular/fire/firestore';
+ */
+ const firebaseConfig = environment.firebaseConfig;
+initializeApp(firebaseConfig);
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,10 +24,13 @@ import { CategoryComponent } from './category/category.component';
     FooterComponent,
     TodoComponent,
     CategoryComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
